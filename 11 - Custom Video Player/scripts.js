ac0a334748b7f3b9c1f85fc9e34c9c57a616c488
HN <hhnguyen92@gmail.com>
2018-08-27 17:24:18 -0700

    Add play pause functionality with event listeners

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -2,7 +2,25 @@ const player = document.querySelector('.player');
 const video = player.querySelector('.viewer');
 const progress = player.querySelector('.progress');
 const progressBar = player.querySelector('.progress__filled');
-const toggle = player.querySelector('toggle');
+const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
 
+function togglePlay() {
+  if (video.paused) {
+    video.play().then(() => {
+      
+    });
+  } else {
+    video.pause();
+  }
+}
+
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+toggle.addEventListener('click', togglePlay);
